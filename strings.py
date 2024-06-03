b399c99b1371fa497530693da2edf891fd1ad7f4
EmadNama <emadnama@hotmail.com>
2024-06-03 16:16:52 +0300

    Lesson 2, 03/06/2024

---
+++ strings.py
@@ -0,0 +1,15 @@
+'''
+name=input("Please enter a name:")
+print(name[0], name[-1])
+print(name[1:3])
+print("Hello"+'/n'"My name is Emad")
+print(name[1:])
+print(len(name))
+
+
+fullname=str(input("Please put your full name: "))
+print(fullname.upper())
+print(fullname.lower())
+print(len(fullname))
+middleIndex=len(fullname)//2
+print(fullname[middleIndex])'''
