c725a00fa274ce2f4600419b5627013a6b47ee14
EmadNama <emadnama@hotmail.com>
2024-06-04 19:00:55 +0300

    Lesson 3, conditions

--- conditions.py
+++ conditions.py
@@ -39,13 +39,45 @@ else:print("F")
 
 '''
 
-ticket=int(input("Ticket price: "))
+
+'''
+ticket=float(input("Ticket price: "))
 age=int(input("Age: "))
-if 0<age<18:
+if 0<=age<18:
     print(f"You got 50% discount!!! please pay {ticket*0.50}")
-elif 18<=age<=30:
+elif 18<=age<30:
     print(f"You have to pay full price which is {ticket}")
-elif 30<age<64:
+elif 30<=age<64:
     print(f"You got 20% discount!!! please pay {ticket*0.80}")
 elif age>=64:
     print(f"You got 80% discount!!! please pay {ticket*0.20}")
+    
+'''
+
+'''
+
+num=int(input("Put a two digits number: "))
+if num%2==0:
+    if num%5==0:
+        print("The number is divided by 2 and 5 so also by 10")
+    else: print("The number is divided by 2 but not 5")
+else: print("The number is odd")
+
+'''
+
+threedigits=int(input("Put a three digits number: "))
+if not 100<=threedigits<=999:
+    print("Bye bye!")
+if 100<=threedigits<=999:
+    if threedigits%2==0:
+        print("The number can be divided by 2")
+    if not threedigits%2==0:
+       print("The number is odd")
+       print(f"The closest even number is {threedigits-1} or {threedigits+1}")
+       print(f"The first digit is: {threedigits//100}, Second digit: {threedigits%100//10}, Last digit: {threedigits%10}")
+    digit1=threedigits//100
+    digit2=threedigits%100//10
+    digit3=threedigits%10
+    if digit1+digit3<digit2:
+        print("The first digit and last digit sum is less than the middle digit")
+    else:print(f"the first digit and last digit sum is more than the middle digit and their multiplication is {digit1*digit3}")
