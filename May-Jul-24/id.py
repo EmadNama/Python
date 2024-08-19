b9921ded789aaea4742777a77d6162528d9ca040
EmadNama <emadnama@hotmail.com>
2024-08-19 19:31:58 +0300

    id updated

--- May-Jul-24/id.py
+++ May-Jul-24/id.py
@@ -1,31 +1,48 @@
 class ID:
+
     def __init__(self, id):
         self.id = id
 
     def IdWithLastDigit(self):
+
         if len(self.id) > 8:
             return("8 Digits only!")
         elif len(self.id) < 8:
             while len(self.id) < 8:
                 self.id = "0" + self.id
-        sum = 0
-        for digit in (self.id[1::2]):
-            digit = int(digit)
-            digit = digit * 2
-            if digit >= 10:
-                digit = (digit%10) + (digit//10)
-                sum += digit
+
+        temp_sum = 0
+        for digit in self.id[1::2]:
+            if (int(digit) * 2) > 9:
+                temp_sum += (int(digit) * 2) - 9
             else:
-                sum += (digit)
-        for digit in (self.id[0::2]):
-            sum+=int(digit)
-        lastdigit = (10 - sum%10)
-        return self.id + str(lastdigit)
+                temp_sum += int(digit) * 2
+
+        for digit in self.id[0::2]:
+            temp_sum += int(digit)
+
+        return self.id + str(10 - temp_sum%10)
+
+    def IdCheck(self):
+        if self.id == ID(self.id[:8]).IdWithLastDigit():
+            return "Legal ID"
+        else:
+            return "Not legal"
 
 
-
-
+# 32306989-8
 newid = ID("32306989")
 print(newid.IdWithLastDigit())
+newid = ID("323069898")
+print(newid.IdCheck())
 
 
+
+
+
+
+# class Person:
+#     def __init__(self, firstname, lastname, id):
+#         self.firstname = firstname
+#         self.lastname = lastname
+#         self.id = id
